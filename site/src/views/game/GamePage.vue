eb07b943e34acf5a32c2dce9bf81a245a45a3f7c
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-08-06 10:30:23 +0800

    Update GamePage.vue

--- site/src/views/game/GamePage.vue
+++ site/src/views/game/GamePage.vue
@@ -4,12 +4,13 @@ import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/user';
 import VueRequest from '@/vue-request';
-import QRCodeVue3 from 'qrcode-vue3';
+import QrcodeContent from '@/components/game/QrcodeContent.vue';
 
 const { t, locale } = useI18n();
 const router = useRouter();
 const user = useUserStore();
 const vr = new VueRequest(user.token);
+const displayModal = ref(0);
 
 </script>
 
@@ -18,25 +19,11 @@ const vr = new VueRequest(user.token);
     <div class="m-auto w-full sm:w-96 h-full flex flex-col">
       <div class="flex-grow"></div>
       <div class="bg-white p-10 sm:p-5 sm:shadow">
-        <div class="text-center">
-          <QRCodeVue3
-            :width="260"
-            :height="260"
-            value="https://scholtz.sk"
-            :qrOptions="{ typeNumber: 5, mode: 'Byte', errorCorrectionLevel: 'H' }"
-            :imageOptions="{ hideBackgroundDots: true, imageSize: 0.4, margin: 0 }"
-            :dotsOptions="{
-              type: 'extra-rounded',
-              color: '#000',
-            }"
-            fileExt="png"
-            :image="'./src/assets/logo_only.svg'"
-          />
+        <div class="py-5 text-center">
+          <div class="text-3xl">{{ user.userInfo.name }}</div>
+          <div class="text-xl">{{ user.userInfo.school_id }}</div>
         </div>
-        <div class="py-5">
-          <div class="text-2xl">{{ user.userInfo.name }}</div>
-          <div>{{ user.userInfo.school_id }}</div>
-        </div>
+        <QrcodeContent></QrcodeContent>
       </div>
       <div class="flex-grow"></div>
     </div>
