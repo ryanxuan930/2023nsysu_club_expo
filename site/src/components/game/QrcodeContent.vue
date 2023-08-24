dddc53c873b1cca155b57b97e467035b9bc79db9
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-08-25 02:20:37 +0800

    Update QrcodeContent.vue

--- site/src/components/game/QrcodeContent.vue
+++ site/src/components/game/QrcodeContent.vue
@@ -8,15 +8,17 @@ import { useI18n } from 'vue-i18n';
 const user = useUserStore();
 const vr = new VueRequest(user.token);
 const qrcode: any = ref('');
+const isLoaded = ref(false);
 const { t, locale } = useI18n();
 const counter = ref(0);
 async function getData() {
   await vr.Get('auth/user/qrcode', qrcode, true, true);
+  isLoaded.value = true;
   counter.value = 120;
+  console.log(isLoaded.value);
   console.log(qrcode.value);
-  console.log(1);
 }
-console.log(2);
+console.log(isLoaded.value);
 getData();
 setInterval(() => {
   counter.value--;
@@ -27,7 +29,7 @@ setInterval(() => {
 </script>
 
 <template>
-  <div v-if="qrcode != ''">
+  <div v-if="isLoaded">
     <div class="flex">
       <div class="flex-grow"></div>
       <QRCodeVue3
