2e620fca6fd615d4dbdce2cb4459683c8a590b65
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2023-08-25 02:59:22 +0800

    Update index.ts

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import GameView from '../views/GameView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -179,7 +180,7 @@ const router = createRouter({
     {
       path: '/game',
       name: 'game',
-      component: () => import('../views/GameView.vue'),
+      component: GameView,
       children: [
         {
           path: '',
